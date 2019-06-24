8ab76ffdce9624d93ccff589727ebf76a6057e1a
MaiSaid30 <maiossaid@gmail.com>
2019-06-24 01:26:31 -0700

    create new export.pkl that support some missing libraries. modified requirements.txt

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/file/d/1pkZjH4igdJCJ5_k7Uu3GmBxWGdLGhV1S/view?usp=sharing'
+export_file_url = 'https://drive.google.com/file/d/1QsY8gXzyYi_-v4wfiaTU2lmpEuEtSnxs/view?usp=sharing'
 #export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
