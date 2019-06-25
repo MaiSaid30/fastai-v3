8b4c6c5282cf61ed19b25f5513d3062d2e5cc3db
MaiSaid30 <maiossaid@gmail.com>
2019-06-24 23:51:55 -0700

    use the google generated link instead of google shared link in server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/file/d/1UY_O4Wrda3WOvRwAdjJAOnhxNxoNnw4V/view?usp=sharing'
+#export_file_url = 'https://drive.google.com/file/d/1UY_O4Wrda3WOvRwAdjJAOnhxNxoNnw4V/view?usp=sharing'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1UY_O4Wrda3WOvRwAdjJAOnhxNxoNnw4V?alt=media&key=AIzaSyCqPALKBFwX4rpEEYZ6y3jyvYfY0I4D2KU'
 #export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
